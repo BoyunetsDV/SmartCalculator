d775986ea7ca480e17afeb39a68125b9ed012fa5
Boiunets Denys <boyunets.dv@gmail.com>
2020-10-09 10:18:05 +0300

    first step completed

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,5 +1,10 @@
 package calculator
 
+import java.util.*
+
 fun main() {
-    println("Hello, World!")
+    val scanner = Scanner(System.`in`)
+    val num1 = scanner.nextInt()
+    val num2 = scanner.nextInt()
+    print(num1 + num2)
 }
