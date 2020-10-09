d101cf787fdbd20afc84a3610c1209755d71ffcf
Boiunets Denys <boyunets.dv@gmail.com>
2020-10-09 12:20:39 +0300

    thhird step completed

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -6,11 +6,18 @@ fun main() {
     val scanner = Scanner(System.`in`)
     do {
         val input = scanner.nextLine()
-        if (input.isEmpty() || input == "/exit") {
+        if (isAdditionalCommand(input)) {
+            executeAdditionalCommand(input)
             continue
         }
 
-        val numbers = input.split(" ").map { it -> it.toInt() }
+        if (isTheInputStringInvalid(input)) {
+            continue
+        }
+
+        val numbers = input.split(" ")
+                .filter { it -> it.isNotEmpty() }
+                .map { it -> it.toInt() }
         if (numbers.size == 1) {
             println(numbers[0])
             continue
@@ -20,6 +27,25 @@ fun main() {
     println("Bye!")
 }
 
+fun isTheInputStringInvalid(input: String): Boolean {
+    return input.isEmpty()
+}
+
+fun isAdditionalCommand(input: String): Boolean {
+    val commands = listOf("/exit", "/help")
+    return commands.contains(input)
+}
+
+fun executeAdditionalCommand(input: String) {
+    when (input) {
+        "/help" -> println("The program calculates the sum of numbers")
+    }
+}
+
 fun calculate(numbers: List<Int>) {
-    println(numbers[0] + numbers[1])
+    var result = 0
+    for (number in numbers) {
+        result += number
+    }
+    println(result)
 }
