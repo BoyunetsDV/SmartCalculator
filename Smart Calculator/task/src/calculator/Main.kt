7faa64d31782010d327df41ce82ba1b4ca2839d3
Boiunets Denys <boyunets.dv@gmail.com>
2020-10-09 11:54:51 +0300

    second step completed

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -4,7 +4,22 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    val num1 = scanner.nextInt()
-    val num2 = scanner.nextInt()
-    print(num1 + num2)
+    do {
+        val input = scanner.nextLine()
+        if (input.isEmpty() || input == "/exit") {
+            continue
+        }
+
+        val numbers = input.split(" ").map { it -> it.toInt() }
+        if (numbers.size == 1) {
+            println(numbers[0])
+            continue
+        }
+        calculate(numbers)
+    } while (input != "/exit")
+    println("Bye!")
+}
+
+fun calculate(numbers: List<Int>) {
+    println(numbers[0] + numbers[1])
 }
